a517f1132369126bf9a884ec60f9e77ae4031987
xnx3 <mail@xnx3.com>
2021-01-08 13:35:56 +0800

    优化包扫描

--- src/main/java/com/xnx3/ScanClassUtil.java
+++ src/main/java/com/xnx3/ScanClassUtil.java
@@ -221,7 +221,7 @@ public class ScanClassUtil {
                             Thread.currentThread().getContextClassLoader().loadClass(packageName + '.' + className));
                 } catch (ClassNotFoundException e) {
                     // log.error("添加用户自定义视图类错误 找不到此类的.class文件");
-                    e.printStackTrace();
+                    //e.printStackTrace();
                 }
             }
         }
